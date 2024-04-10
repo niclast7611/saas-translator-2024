764815fc357276788d6b40c55bdb5c1966b9c9d5
Nic Last <nic@ripemetrics.com>
2024-04-09 17:50:47 -0700

    Update firebase configuration to use environment variables

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGczx3k0K2UtLTBdpkzx3boItkUQF0xIw",
-  authDomain: "saas-translator-2024.firebaseapp.com",
-  projectId: "saas-translator-2024",
-  storageBucket: "saas-translator-2024.appspot.com",
-  messagingSenderId: "152138637442",
-  appId: "1:152138637442:web:f6622b11e639975fe3549b"
+  apiKey: String(process.env.FIREBASE_API_KEY!),
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.FIREBASE_APP_ID!
 };
+
   const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
   const auth = getAuth(app)
   const db = getFirestore(app)
